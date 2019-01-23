8656aad5098534550c8bf763d3f2c97130c941c1
Marian Ciortea <marian.ciortea@yahoo.com>
2019-01-23 21:39:50 +0200

    Enable seeding database with default user.

---
+++ src/Infrastructure/ApplicationDbContextSeed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class ApplicationDbContextSeed
+    {
+        public static async Task SeedAsync(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        {
+            await EnsureUser(userManager, "test@domain.com", "P@ssword1");
+        }
+
+        private static async Task EnsureUser(UserManager<IdentityUser> userManager, string userName, string password)
+        {
+            var user = await userManager.FindByNameAsync(userName);
+            if (user == null)
+            {
+                user = new IdentityUser { UserName = userName, Email = userName };
+                await userManager.CreateAsync(user, password);
+            }
+        }
+    }
+}
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using Infrastructure;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Web
@@ -14,7 +12,27 @@ namespace Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var db = services.GetRequiredService<ApplicationDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                    ApplicationDbContextSeed.SeedAsync(db, userManager).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occurred seeding the database");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
