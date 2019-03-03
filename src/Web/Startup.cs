fa04cacd22e9ec114c4bb5127b3f45a8c1cbaa96
Marian Ciortea <marian.ciortea@yahoo.com>
2019-03-03 21:51:16 +0200

    Add bundler and minifier package. Move bootstrap-datepicker to SelectStayPeriod.cshtml.

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -38,9 +38,11 @@ namespace Web
         public void ConfigureProductionServices(IServiceCollection services)
         {
             // Use real database
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            ConfigureServices(services);
+            //ConfigureServices(services);
+
+            ConfigureTestingServices(services);
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
